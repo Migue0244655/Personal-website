5c2130a0509f97238beca40200d15e7235441935
Migue0244655 <112912731+Migue0244655@users.noreply.github.com>
2022-10-07 14:13:18 -0500

    Add files via upload

---
+++ Tarea_node/external_modules_1/index.js
@@ -0,0 +1,7 @@
+var hero = require("superheroes");
+var hero_name = hero.random();
+
+var villian = require("supervillains");
+var villian_name = villian.random();
+
+console.log("- Look it is " + villian_name + " ready to do its evil deeds!\n- Who could save us!\n- Fear not! Here is " + hero_name + " to the rescue!");
